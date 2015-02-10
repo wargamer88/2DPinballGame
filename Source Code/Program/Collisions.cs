2613dac2b2e7998c35160eb06fac3f927e4c9761
RGravity <rogieroh@hotmail.com>
2015-02-10 13:12:29 +0100

    Collision.cs

---
+++ Source Code/Program/Collisions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class Collisions
+    {
+        public Collisions()
+        {
+        }
+        
+        public Ball OuterCircleCollisionTest(OuterCircle OC, Ball ball)
+        {
+            Vec2 Difference = OC.position.Clone().Sub(ball.position.Clone());
+            float distance = Difference.Length();
+
+            if (distance > (ball.radius + OC.radius))
+            {
+                float separation = ball.radius + OC.radius - distance;
+                Vec2 normal = Difference.Normalize();
+                Vec2 impulse = normal.Clone().Scale(separation);
+
+                ball.position.Sub(impulse);
+                ball.velocity.Reflect(normal);
+            }
+            return ball;
+        }
+    }
+}
