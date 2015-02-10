8cf9a97f9e1e3ef8ca47aaae02c9e0c2db4b88fe
wargamer88 <m.van.denzel@home.nl>
2015-02-10 16:34:04 +0100

    syncing my shit

--- Source Code/Program/Orbs.cs
+++ Source Code/Program/Orbs.cs
@@ -13,14 +13,38 @@ namespace GXPEngine
         const float orbMovingSpeed = 1f;
         const float orbAcceleration = 0.0001f;
 
+        //resolution on 1366 x 768
+        private Vec2 positionTop;
+        private Vec2 positionBottom;
+        private Vec2 positionRight;
+        private Vec2 positionLeft;
+        private Vec2 positionTopLeft;
+        private Vec2 positionTopRight;
+        private Vec2 positionBottomLeft;
+        private Vec2 positionBottomRight;
+
         public Orbs(Game game) : base(game.width,game.height)
         {
             _orbList = new List<Ball>();
 
+            positionTop = new Vec2(1366 / 2, -30);
+            positionBottom = new Vec2(1366 / 2, 768 + 30);
+            positionRight = new Vec2((1366 / 2) + (768 / 2) + 30, 768 / 2);
+            positionLeft = new Vec2((1366 / 2) - (768 / 2) - 30, 768 / 2);
+
             //red - fire orb
-            _orb = new Ball(30, new Vec2(game.width / 2, 0), new Vec2(0, orbMovingSpeed), new Vec2(0, orbAcceleration), Color.Red);
+            _orb = new Ball(30, positionLeft, new Vec2(0, orbMovingSpeed), new Vec2(0, orbAcceleration), Color.Red);
             _orbList.Add(_orb);
             AddChild(_orb);
+
+            //gray - wind orb
+            _orb = new Ball(30, new Vec2(game.width / 2, 0), new Vec2(0, orbMovingSpeed), new Vec2(0, orbAcceleration), Color.Gray);
+            _orbList.Add(_orb);
+            AddChild(_orb);
+
+            //cyan - lightning orb
+
+            //brown - earth orb
 
             //blue - water orb
             _orb = new Ball(30, new Vec2(game.width, game.height / 2), new Vec2(-orbMovingSpeed, 0), new Vec2(-orbAcceleration, 0), Color.Blue);
