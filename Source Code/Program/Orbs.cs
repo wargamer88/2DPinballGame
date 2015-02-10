b0aac0710a9106d482b54807853046ab8beacf49
wargamer88 <m.van.denzel@home.nl>
2015-02-10 14:34:51 +0100

    trying adding orbs

--- Source Code/Program/Orbs.cs
+++ Source Code/Program/Orbs.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace GXPEngine
 {
-    class Orbs : GameObject
+    class Orbs : Canvas
     {
-        public Orbs()
-        {
+        public List<Ball> _orbList;
+        Ball _orb;
 
+        public Orbs(Game game) : base(game.width,game.height)
+        {
+            _orbList = new List<Ball>();
+
+            //red - fire orb
+            _orb = new Ball(30, new Vec2(game.width / 2, 0), new Vec2(0, 1), new Vec2(0,1), Color.Red);
+            _orbList.Add(_orb);
+            AddChild(_orb);
         }
 
     }
