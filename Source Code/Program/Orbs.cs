0019afe7e358e67586c717ef9a1648ad6eecdde6
wargamer88 <m.van.denzel@home.nl>
2015-02-11 11:25:33 +0100

    Revert "added orb positions, velocities and accelerations"
    This reverts commit 137917ce89a4bea7ce3806ea0de2d8a5dee9f6c1.

--- Source Code/Program/Orbs.cs
+++ Source Code/Program/Orbs.cs
@@ -23,77 +23,31 @@ namespace GXPEngine
         private Vec2 positionBottomLeft;
         private Vec2 positionBottomRight;
 
-        private Vec2 velocityTopToBottom;
-        private Vec2 velocityBottomToTop;
-        private Vec2 velocityRightToLeft;
-        private Vec2 velocityLeftToRight;
-        private Vec2 velocityTopLeftToBottomRight;
-        private Vec2 velocityTopRightToBottomLeft;
-        private Vec2 velocityBottomLeftToTopRight;
-        private Vec2 velocityBottomRightToTopLeft;
-
-        private Vec2 accelerationTopToBottom;
-        private Vec2 accelerationBottomToTop;
-        private Vec2 accelerationRightToLeft;
-        private Vec2 accelerationLeftToRight;
-        private Vec2 accelerationTopLeftToBottomRight;
-        private Vec2 accelerationTopRightToBottomLeft;
-        private Vec2 accelerationBottomLeftToTopRight;
-        private Vec2 accelerationBottomRightToTopLeft;
-
         public Orbs(Game game) : base(game.width,game.height)
         {
             _orbList = new List<Ball>();
 
-            positionTop                     = new Vec2(1366 / 2, -30);
-            positionBottom                  = new Vec2(1366 / 2, 768 + 30);
-            positionRight                   = new Vec2((1366 / 2) + (768 / 2) + 30, 768 / 2);
-            positionLeft                    = new Vec2((1366 / 2) - (768 / 2) - 30, 768 / 2);
-            positionTopLeft                 = new Vec2(1366 / 8 * 3, 768 / 8);
-            //positionTopRight                = new Vec2
-            //positionBottomLeft              = new Vec2
-            //positionBottomRight             = new Vec2
-
-            velocityTopToBottom             = new Vec2(0, orbMovingSpeed);
-            velocityBottomToTop             = new Vec2(0, -orbMovingSpeed);
-            velocityRightToLeft             = new Vec2(-orbMovingSpeed, 0);
-            velocityLeftToRight             = new Vec2(orbMovingSpeed, 0);
-            velocityTopLeftToBottomRight    = new Vec2(new Vec2(orbMovingSpeed, orbMovingSpeed).Length(), new Vec2(orbMovingSpeed, orbMovingSpeed).Length());
-            //velocityTopRightToBottomLeft    = new Vec2
-            //velocityBottomLeftToTopRight    = new Vec2
-            //velocityBottomRightToTopLeft    = new Vec2
-
-            accelerationTopToBottom         = new Vec2(0, orbAcceleration);
-            accelerationBottomToTop         = new Vec2(0, -orbAcceleration);
-            accelerationRightToLeft         = new Vec2(-orbAcceleration, 0);
-            accelerationLeftToRight         = new Vec2(orbAcceleration, 0);
-            accelerationTopLeftToBottomRight = new Vec2(new Vec2(orbAcceleration, orbAcceleration).Length(), new Vec2(orbAcceleration, orbAcceleration).Length());
-            //accelerationTopRightToBottomLeft= new Vec2
-            //accelerationBottomLeftToTopRight= new Vec2
-            //accelerationBottomRightToTopLeft= new Vec2
+            positionTop = new Vec2(1366 / 2, -30);
+            positionBottom = new Vec2(1366 / 2, 768 + 30);
+            positionRight = new Vec2((1366 / 2) + (768 / 2) + 30, 768 / 2);
+            positionLeft = new Vec2((1366 / 2) - (768 / 2) - 30, 768 / 2);
 
             //red - fire orb
-            _orb = new Ball(30, positionTop, velocityTopToBottom, accelerationTopToBottom, Color.Red);
+            _orb = new Ball(30, positionLeft, new Vec2(0, orbMovingSpeed), new Vec2(0, orbAcceleration), Color.Red);
             _orbList.Add(_orb);
             AddChild(_orb);
 
             //gray - wind orb
-            _orb = new Ball(30, positionBottom, velocityBottomToTop, accelerationBottomToTop, Color.Gray);
+            _orb = new Ball(30, new Vec2(game.width / 2, 0), new Vec2(0, orbMovingSpeed), new Vec2(0, orbAcceleration), Color.Gray);
             _orbList.Add(_orb);
             AddChild(_orb);
 
             //cyan - lightning orb
-            _orb = new Ball(30, positionRight, velocityRightToLeft, accelerationRightToLeft, Color.Cyan);
-            _orbList.Add(_orb);
-            AddChild(_orb);
 
             //brown - earth orb
-            _orb = new Ball(30, positionLeft, velocityLeftToRight, accelerationLeftToRight, Color.Brown);
-            _orbList.Add(_orb);
-            AddChild(_orb);
 
             //blue - water orb
-            _orb = new Ball(30, positionTopLeft, velocityTopLeftToBottomRight, accelerationTopLeftToBottomRight, Color.Blue);
+            _orb = new Ball(30, new Vec2(game.width, game.height / 2), new Vec2(-orbMovingSpeed, 0), new Vec2(-orbAcceleration, 0), Color.Blue);
             _orbList.Add(_orb);
             AddChild(_orb);
         }
