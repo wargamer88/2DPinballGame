9403ae338aac746b91b50b4f2c4b2d823910bcba
RGravity <rogieroh@hotmail.com>
2015-02-11 15:12:01 +0100

    Added Random Crap

---
+++ Source Code/Program/Spawn.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    class Spawn
+    {
+        public static Color RandomColor()
+        {
+            int random = Utils.Random(1, 6);
+
+            switch (random)
+            {
+                case 1:
+                    return Color.Red;
+                case 2:
+                    return Color.White;
+                case 3:
+                    return Color.Blue;
+                case 4:
+                    return Color.Brown;
+                case 5:
+                    return Color.Cyan;
+                default:
+                    return Color.Black;
+            }
+            
+        }
+
+        public static enumBallPositions RandomPosition()
+        {
+            int random = Utils.Random(1, 9);
+
+            switch (random)
+            {
+                case 1:
+                    return enumBallPositions.Bottom;
+                case 2:
+                    return enumBallPositions.BottomLeft;
+                case 3:
+                    return enumBallPositions.BottomRight;
+                case 4:
+                    return enumBallPositions.Left;
+                case 5:
+                    return enumBallPositions.Right;
+                case 6:
+                    return enumBallPositions.Top;
+                case 7:
+                    return enumBallPositions.TopLeft;
+                case 8:
+                    return enumBallPositions.TopRight;
+                default:
+                    return enumBallPositions.Bottom;
+            }
+        }
+    }
+}
